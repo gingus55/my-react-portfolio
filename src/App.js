c8155e432eeca10a7df547a5f59e074d9a055d38
gingus55 <gingus55@hotmail.com>
2022-02-14 12:01:51 +0000

    aside size play

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router";
 import { HomePage } from "./Pages/HomePage";
 import { ContactPage } from "./Pages/ContactPage";
 import { ProjectPage } from "./Pages/ProjectPage";
+
 // import { Header } from "./Components/Header/Header";
 
 function App() {
--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,14 +1,19 @@
 import React from "react";
 import { Banner } from "../Components/Banner/Banner";
 import { Header } from "../Components/Header/Header";
-import { ProjectCard } from "../Components/ProjectCard/ProjectCard";
+
+import "./MainPage.css";
 
 export const HomePage = () => {
   return (
     <>
       <Header />
       <Banner />
-      <ProjectCard />
+      {/* <ProjectCard /> */}
+      <div className="flex-container">
+        <div className="left-aside"></div>
+        <div className="right-side"></div>
+      </div>
     </>
   );
 };
