e0c053b0ab2f0d541ff4bcd3a6391c06149c1f93
gingus55 <gingus55@hotmail.com>
2022-02-14 16:23:19 +0000

    form added

--- src/Pages/ContactPage.js
+++ src/Pages/ContactPage.js
@@ -1,11 +1,12 @@
 import React from "react";
+import { Form } from "../Components/Form/Form";
 import { Header } from "../Components/Header/Header";
 
 export const ContactPage = () => {
   return (
     <>
       <Header />
-      <div>Contact Page</div>
+      <Form />
     </>
   );
 };
