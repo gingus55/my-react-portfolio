c629cbd4f649faa1e359e242aca1557669ce8f3e
gingus55 <gingus55@hotmail.com>
2022-02-19 16:57:29 +0000

    banner styling

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -1,11 +1,21 @@
 import { Image } from "@mui/icons-material";
-import { Grid } from "@mui/material";
+import { Grid, Typography } from "@mui/material";
 import pixelChris from "../../assets/images/PixelChris.jpg";
+
+import "./index.css";
 
 export const Banner = () => {
   return (
-    <Grid container spacing={2}>
-      <Image src={pixelChris} />
+    <Grid sx={{ paddingLeft: "24px", marginTop: "10px" }} container spacing={2}>
+      <Grid item xs={6}>
+        <img className="pixel" src={pixelChris} />
+      </Grid>
+      <Grid item xs={6}>
+        <Typography textAlign="center" p="10px" m="10px">
+          Maths teacher of 13 years looking to learn something new. Always had
+          an interest in coding since writing bootdisks for games on my 486!
+        </Typography>
+      </Grid>
     </Grid>
   );
 };
--- src/Pages/ProjectPage.js
+++ src/Pages/ProjectPage.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Header } from "../Components/Header/Header";
+import { ProjectCard } from "../Components/ProjectCard/ProjectCard";
 
 export const ProjectPage = () => {
   return (
     <>
       <Header />
       <div>Projects Page</div>
+      <ProjectCard />
     </>
   );
 };
