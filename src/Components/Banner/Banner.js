4fb4dd05cebd5b137506d0a9eb0891fed7315054
gingus55 <gingus55@hotmail.com>
2022-02-19 11:19:18 +0000

    custom banner started

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -1,33 +1,11 @@
-// import { MDCBanner } from "@material/banner";
-
-// const banner = new MDCBanner(document.querySelector(".mdc-banner"));
+import { Image } from "@mui/icons-material";
+import { Grid } from "@mui/material";
+import pixelChris from "../../assets/images/PixelChris.jpg";
 
 export const Banner = () => {
   return (
-    <div className="mdc-banner" role="banner">
-      <div
-        className="mdc-banner__content"
-        role="alertdialog"
-        aria-live="assertive"
-      >
-        <div className="mdc-banner__graphic-text-wrapper">
-          <div className="mdc-banner__graphic" role="img" alt="">
-            <i className="material-icons mdc-banner__icon">error_outline</i>
-          </div>
-          <div className="mdc-banner__text">
-            There was a problem processing a transaction on your credit card.
-          </div>
-        </div>
-        <div className="mdc-banner__actions">
-          <button
-            type="button"
-            className="mdc-button mdc-banner__primary-action"
-          >
-            <div className="mdc-button__ripple"></div>
-            <div className="mdc-button__label">Fix it</div>
-          </button>
-        </div>
-      </div>
-    </div>
+    <Grid container spacing={2}>
+      <Image src={pixelChris} />
+    </Grid>
   );
 };
