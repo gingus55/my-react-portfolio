ed69f4a5f87b9efb732f17cd44d05d4c6ba141e6
gingus55 <gingus55@hotmail.com>
2022-02-19 10:39:21 +0000

    reactive side-bar

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,22 +1,22 @@
+import { Grid } from "@mui/material";
 import React from "react";
 import { Banner } from "../Components/Banner/Banner";
 import { Header } from "../Components/Header/Header";
 import { LeftAside } from "../Components/LeftAside/LeftAside";
-
-import "./MainPage.css";
 
 export const HomePage = () => {
   return (
     <>
       <Header />
       <Banner />
-      {/* <ProjectCard /> */}
-      <div className="flex-container">
-        <div className="left-aside">
+      <Grid container spacing={1}>
+        <Grid item xs={2}>
           <LeftAside />
-        </div>
-        <div className="right-side"></div>
-      </div>
+        </Grid>
+        <Grid item xs={10}>
+          <div className="right-side"></div>
+        </Grid>
+      </Grid>
     </>
   );
 };
