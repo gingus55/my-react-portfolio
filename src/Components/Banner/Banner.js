dff4dfa50a5efc0f419c19f5310bf082b42ef665
gingus55 <gingus55@hotmail.com>
2022-02-21 14:43:32 +0000

    banner spacing sorted

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -7,17 +7,22 @@ import "./index.css";
 export const Banner = () => {
   return (
     <Grid sx={{ paddingLeft: "24px", marginTop: "10px" }} container spacing={2}>
-      <Grid item xs={6}>
+      <Grid item xs={6} sx={{ padding: "20px" }}>
         <img className="pixel" src={pixelChris} alt="pixel of Chris Bradshaw" />
       </Grid>
-      <Grid item xs={6}>
-        <Typography textAlign="center" pr="24%" pt="24%">
+      <Grid
+        container
+        direction="column"
+        justifyContent="space-around"
+        alignItems="center"
+        xs={6}
+        justify="center"
+      >
+        <Typography textAlign="center">
           Maths teacher of 13 years looking to learn something new. Always had
           an interest in coding since writing bootdisks for games on my 486!
         </Typography>
         <Button
-          sx={{ ml: 10, mr: 10, mt: 1 }}
-          textAlign="center"
           variant="outlined"
           onClick={() => {
             window.location.replace("http://localhost:3000/Contact");
