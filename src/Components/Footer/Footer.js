a05f04f0e5b7d27ccfaf234dc015e579ae7321c1
gingus55 <gingus55@hotmail.com>
2022-02-27 13:58:37 +0000

    footer alignment

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -5,33 +5,37 @@ import cvIcon from "../../assets/images/cv-icon.png";
 
 export const Footer = () => {
   return (
-    <Grid
-      container
-      direction="row"
-      justifyContent="space-around"
-      alignItems="center"
-    >
-      <img
-        onClick={() => {
-          window.location.href =
-            "https://linkedin.com/in/chris-bradshaw-39549421a";
-        }}
-        className="icon"
-        src={linkedInIcon}
-        alt="linkedInLogo"
-      />
+    <Grid item xs={12} textAlign="center">
+      <Grid
+        container
+        direction="row"
+        justifyContent="space-around"
+        alignItems="center"
+      >
+        <img
+          onClick={() => {
+            window.location.href =
+              "https://linkedin.com/in/chris-bradshaw-39549421a";
+          }}
+          className="icon"
+          src={linkedInIcon}
+          alt="linkedInLogo"
+        />
 
-      <img
-        onClick={() => {
-          window.location.href = "https://github.com/gingus55";
-        }}
-        className="icon"
-        src={gitHubIcon}
-        alt="linkedInLogo"
-      />
+        <img
+          onClick={() => {
+            window.location.href = "https://github.com/gingus55";
+          }}
+          className="icon"
+          src={gitHubIcon}
+          alt="linkedInLogo"
+        />
 
-      <img className="icon" src={cvIcon} alt="linkedInLogo" />
-      <Typography>Contact Email: gingus55@hotmail.com</Typography>
+        <img className="icon" src={cvIcon} alt="linkedInLogo" />
+      </Grid>
+      <Grid item xs={12}>
+        <Typography>Contact Email: gingus55@hotmail.com</Typography>
+      </Grid>
     </Grid>
   );
 };
