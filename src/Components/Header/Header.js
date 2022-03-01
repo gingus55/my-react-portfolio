31925d570b94ace8f4f40ab7bb9ac5b86bdd8432
Nazeh Abel <talentednazeh@gmail.com>
2022-03-01 21:29:32 +0000

    some fixes

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -23,11 +23,12 @@ export const Header = () => {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
   const [anchorElUser, setAnchorElUser] = React.useState(null);
 
-  const handleOpenNavMenu = (event) => {
-    setAnchorElNav(event.currentTarget);
-  };
   const handleOpenUserMenu = (event) => {
     setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
   };
 
   // const handleCloseNavMenu = (event) => {
@@ -89,12 +90,36 @@ export const Header = () => {
               aria-label="account of current user"
               aria-controls="menu-appbar"
               aria-haspopup="true"
-              onClick={handleOpenNavMenu}
+              onClick={handleOpenUserMenu}
               color="inherit"
             >
               <MenuIcon />
             </IconButton>
             <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+              sx={{
+                display: { xs: "block", md: "none" },
+              }}
+            >
+              {pages.map((setting) => (
+                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                  <Typography textAlign="center">{setting}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+            {/* <Menu
               id="menu-appbar"
               anchorEl={anchorElNav}
               anchorOrigin={{
@@ -116,7 +141,7 @@ export const Header = () => {
                   <Typography textAlign="center">{page}</Typography>
                 </MenuItem>
               ))}
-            </Menu>
+            </Menu> */}
           </Box>
           <Typography
             variant="h6"
--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import { createBrowserHistory } from "history";
 
+const customHistory = createBrowserHistory();
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter history={customHistory}>
     <App />
   </BrowserRouter>,
   document.getElementById("root")
