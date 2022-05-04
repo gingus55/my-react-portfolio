ba6bf219f2423693bdaf13f8aef12d4c0074edbb
gingus55 <gingus55@hotmail.com>
2022-05-04 13:07:51 +0100

    wip

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -50,21 +50,21 @@ export const Header = () => {
 
   const handleCloseUserMenu = (event) => {
     if (event.target.textContent === "contact") {
-      // const url = `https://gingus55.github.io/my-react-portfolio/${event.target.textContent}`;
-      // window.location.href = url;
-      // setAnchorElUser(null);
+      const url = `https://gingus55.github.io/my-react-portfolio/${event.target.textContent}`;
+      window.location.href = url;
+      setAnchorElUser(null);
 
-      <Link to="/contact"></Link>;
+      // <Link component={RouterLink} to="/contact"></Link>;
     } else if (event.target.textContent === "home") {
       const url = `https://gingus55.github.io/my-react-portfolio/`;
       window.location.href = url;
       setAnchorElUser(null);
     } else if (event.target.textContent === "projects") {
-      const url = `https://gingus55.github.io/my-react-portfolio/${event.target.textContent}`;
+      const url = `gingus55.github.io/my-react-portfolio/${event.target.textContent}`;
       window.location.href = url;
       setAnchorElUser(null);
     } else if (event.target.textContent === "about") {
-      const url = `https://gingus55.github.io/my-react-portfolio/${event.target.textContent}`;
+      const url = `gingus55.github.io/my-react-portfolio/${event.target.textContent}`;
       window.location.href = url;
       setAnchorElUser(null);
     } else {
